4d6c94c8b5628d2bc063892ca0e96608415a2224
Ruslan Shakirov <shakirovrrr@outlook.com>
2019-10-10 16:54:47 +0300

    ast.h skeleton

--- ast.h
+++ ast.h
@@ -1,6 +1,42 @@
 #ifndef TOY_AST_H
 #define TOY_AST_H
 
-// code
+typedef struct _ClassMember {
+
+} ClassMember;
+
+typedef struct _ClassMembers {
+
+} ClassMembers;
+
+typedef struct _ClassBody {
+
+} ClassBody;
+
+typedef struct _Extension {
+
+} Extension;
+
+typedef struct _ClassDeclaration {
+
+} ClassDeclaration;
+
+typedef struct _ClassDeclarations {
+
+} ClassDeclarations;
+
+typedef struct _Import {
+
+} Import;
+
+typedef struct _Imports {
+
+} Imports;
+
+typedef struct _CompilationUnit {
+	Imports imports;
+	ClassDeclarations classDeclarations;
+} CompilationUnit;
+
 
 #endif // TOY_AST_H
