e277942e47a6b051c012825fa9678715857c02dd
SkarletSkay <meskarlet123@gmail.com>
2019-10-11 23:23:27 +0300

    main

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
+#include "parser.tab.h"
+#include "lex.h"
+#include "ast.h"
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+
+int
+main(int argc, char **argv) 
+{
+  root_node *r = (root_node *) malloc(sizeof(root_node));
+  r->src = fopen("test.txt", "r");
+  yyscan_t sc;
+
+
+	yylex_init(&sc);
+	yyset_in(r->src, sc);
+	int number = yyparse(sc, r);
+	yylex_destroy(sc);
+
+	if (number == 0) {
+		print_node_sexp(r->root);
+	}
+
+  if (r->root != NULL) {
+		delete_sexp_node(r->root);
+	}
+	fclose(r->src);
+	free(r);
+	return 0;
 }
